34fe9853fd8ffba0bee228016b2fa51d4291b5b1
Marlon D. Rocha <mrocha.solks@digio.com.br>
2023-11-16 13:24:42 -0300

    Created OrderedList component.

---
+++ app/src/main/java/br/com/mdr/animeapp/presentation/components/OrderedList.kt
@@ -0,0 +1,53 @@
+package br.com.mdr.animeapp.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.mdr.animeapp.ui.theme.SMALL_PADDING
+
+@Composable
+fun OrderedList(
+    title: String,
+    items: List<String>,
+    textColor: Color
+) {
+    Column {
+        Text(
+            modifier = Modifier.padding(bottom = SMALL_PADDING),
+            text = title,
+            color = textColor,
+            fontSize = MaterialTheme.typography.labelMedium.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+        items.forEachIndexed { index, item ->
+            Text(
+                modifier = Modifier.alpha(ContentAlpha.medium),
+                text = "${index + 1}. $item",
+                color = textColor,
+                fontSize = MaterialTheme.typography.labelSmall.fontSize
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OrderedListPreview() {
+    OrderedList(
+        title = "Family",
+        items = listOf(
+            "Monkey D. Dragon",
+            "Monkey D. Garp",
+            "Portgas D. Ace",
+            "Sabo"
+        ),
+        textColor = MaterialTheme.colorScheme.onBackground)
+}

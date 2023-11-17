7e2f91ddc03169ca340b6afdce0f6af4dcfe4099
Marlon D. Rocha <mrocha.solks@digio.com.br>
2023-11-17 16:56:50 -0300

    Added RatingWidgetTest.

--- app/src/main/java/br/com/mdr/animeapp/data/local/HeroDatabase.kt
+++ app/src/main/java/br/com/mdr/animeapp/data/local/HeroDatabase.kt
@@ -1,6 +1,8 @@
 package br.com.mdr.animeapp.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import br.com.mdr.animeapp.data.local.dao.HeroDao
@@ -11,6 +13,19 @@ import br.com.mdr.animeapp.domain.model.HeroRemoteKeys
 @Database(entities = [Hero::class, HeroRemoteKeys::class], version = 1)
 @TypeConverters(DatabaseConverter::class)
 abstract class HeroDatabase: RoomDatabase() {
+
+    companion object {
+        fun create(context: Context, useInMemory: Boolean): HeroDatabase {
+            val databaseBuilder = if (useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, HeroDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, HeroDatabase::class.java, "test_database.db")
+            }
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
     abstract fun getHeroDao(): HeroDao
     abstract fun getRemoteKeysDao(): HeroRemoteKeysDao
 }
